9822d76f5b37c339f4b956d0f349ae9f3a50f5fd
LeandroReichardt <163359722+LeandroReichardt@users.noreply.github.com>
2024-05-25 12:43:38 -0300

    exercicio06.py

--- Tarefa04/exercicio06.py
+++ Tarefa04/exercicio06.py
@@ -1,4 +1,13 @@
 valor = int(input("Digite um valor "))
 
-if valor%2:
-    
+if valor <= 1:
+    print("O valor não é primo")
+
+elif valor%2 == 0 and valor!= 2 :
+    print("o valor não é primo")
+
+elif valor%3 == 0 and valor != 3 or valor%5 == 0 and valor != 5 or valor%7 == 0 and valor != 7:
+    print("O valor não é primo")
+
+else:
+    print("o valor é primo")
